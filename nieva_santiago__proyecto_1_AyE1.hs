e6180fb1618c9bbe644b8f782597d8219a449baf
pragmatically-dev <pragmatically.dev@gmail.com>
2023-08-29 01:45:35 -0300

    Avances

--- nieva_santiago__proyecto_1_AyE1.hs
+++ nieva_santiago__proyecto_1_AyE1.hs
@@ -1,7 +1,9 @@
+import GHC.Float (float2Int)
+import GHC.IO.Device (IODevice(dup))
+
 {-
     Nombre:   Santiago Emanuel
     Apellido: NIEVA
-    
 -}
 
 -- PROYECTO 1
@@ -393,4 +395,210 @@ ghci> esPrimo 24    Output: False
 
 factorial' :: Int -> Int
 factorial' x = productoria [1..x]
+{-
+Ejecucion:
+ghci> factorial' 5  Output: 120
+ghci> factorial' 1  Output: 1
+ghci> factorial' 0  Output: 1
+ghci> factorial' 2  Output: 2
+ghci> factorial' 3  Output: 6
+-}
+--------------------------------------------------------------------------------------------
+
+-- g) multiplicar primos
+
+{-
+Analisis:
+    Debo recorrer la lista xs, donde si x es primo => x:[]
+    Una vez obtenida la sublista de primos, debo hacer la productoria de los mismos
+
+Herramienta:
+Listas por comprensión
+Existe un mecanismo poderoso para definir listas, similar a la definición de conjuntos por comprensión.
+Veamos los siguientes ejemplos:
+[0, 2, 4, 6] = [2 * x | x ← [0, 1, 2, 3] ]
+[4, 16, 36, 64, 100] = [x * x | x ← [1..10], x par]
+[(1, 1), (1, 2), (1, 3)] = [(a, b) | a ← [1], b ← [1, 2, 3] ]
+El símbolo x ← [0, 1, 2, 3] se lee "x viene de la lista [0,1,2,3]" y se lo denomina generador. Como se puede ver
+en los ejemplos, una definición por comprensión es de la forma [e | Q]; donde e determina la forma de los valores
+que se incluirán en la lista, y Q es una secuencia de generadores y/o predicados que determina a partir de
+qué valores se formarán esos elementos. Un generador indica de qué lista se toman los elementos y un predicado
+determina qué elementos de la lista considerada son elegidos. Siempre debe haber al menos un generador.
+Algunas abreviaciones de listas muy útiles para utilizar para definir listas por comprensión son [n..m] y [n..],
+donde n, m son números enteros. La primera representa la lista de todos los números entre n y m, y la segunda
+la lista de todos los números más grandes que n. Existen otras abreviaciones que son interesantes, juega con
+Haskell para descubrir cómo funcionan. Por ejemplo, ¿qué lista está representada por [2, 4..]?
+Las ventajas de notación por comprensión son dos: es fácil de leer y su escritura es muy parecida a la de
+teoría de conjuntos.
+
+-}
+identidad::Int->Int
+identidad x = x
+
+multiplicarPrimos :: [Int] -> Int
+multiplicarPrimos xs = productoria' [x | x <- xs, esPrimo x ] identidad
+
+{-
+Ejecucion:
+ghci> multiplicarPrimos [3,5,6,9]       Output: 15
+ghci> multiplicarPrimos [3,5,6,9,11]    Output: 165
+-}
+----------------------------------------------------------------------------------------------
+
+-- h) esFib
+
+{-
+Utilizando la formula explicita de Binet [https://es.wikipedia.org/wiki/Sucesi%C3%B3n_de_Fibonacci]
+phi = (1 + sqrt(5)) / 2
+f n = (phi ^n - (-phi^(-1))^n ) / sqrt(5)
+-}
+--IMPORTANTE: Se requiere importar [ GHC.Float (float2Int) ]
+phi =  (1 + sqrt 5 ) / 2
+fib n = float2Int((phi ^n - (-phi^^(-1))^n ) / sqrt 5)
+
+--esFib ::Int->Bool
+esFib::Int -> Bool
+esFib n = n `pertenece` [fib x| x<-[0..n+1]]
+{-
+Ejecucion:
+    ghci> esFib 3   Output: True
+    ghci> esFib 4   Output: False
+    ghci> esFib 5   Output: True
+    ghci> esFib 50  Output: False
+-}
+
+---------------------------------------------------------------------------------------------
+
+-- i) todosFib
+
+todosFib::[Int] -> Bool 
+todosFib xs = paraTodo' xs esFib
+{-
+Ejecucion:
+ghci> todosFib [3]      Output: True  
+ghci> todosFib [3,5]    Output: True
+ghci> todosFib [3,5,7]  Output: False
+ghci> todosFib [3,5,11] Output: False
+ghci> todosFib [3,5,13] Output: True
+-}
+
+----------------------------------------------------------------------------------------------
+
+{-
+Ejercicio 7:
+La función [map] en Haskell se utiliza para aplicar una función dada a cada elemento de una lista, 
+creando asi una nueva lista con los resultados de dichas aplicaciones. Por ejemplo, 
+si tienes la función succ n = n + 1, la expresión map succ [1, -4, 6, 2, -8] 
+aplicaría la función succ a cada elemento de la lista, 
+
+                            resultando en [2, -3, 7, 3, -7].
+
+La función [filter] en Haskell se utiliza para filtrar los elementos de una lista según una condición dada. 
+Solo los elementos que cumplan con la condición se mantienen en la lista resultante. 
+Por ejemplo, si defines la función esPositivo n = n > 0, la expresión filter esPositivo [1, -4, 6, 2, -8] 
+mantendría solamente los elementos positivos en la lista, 
+                        
+                            resultando en [1, 6, 2].
+-}
+
+
+-- map succ [1,-4,6,2,-8]  equivale a:
+succ'::Int->Int
+succ' x =x+1
+
+map'' :: (Int->Int)->[Int]->[Int]
+map'' _ [] = []
+map'' predicadoMap'' (x:xs) = predicadoMap'' x : map'' predicadoMap'' xs
+
+-- filter esPositivo [1, -4, 6, 2, -8]
+filter'' :: (Int->Bool) -> [Int] -> [Int]
+filter'' _ [] = []
+filter'' predicadoFilter'' (x:xs) | predicadoFilter'' x = x:filter'' predicadoFilter'' xs
+                                  | otherwise = filter'' predicadoFilter'' xs
+--------------------------------------------------------------------------------------------------
+
+{-
+Ejercicio 8: 
+Programa una funcion que dada una lista de numeros xs, devuelve la lista que resulta de
+duplicar cada valor de xs.
+    a) Definila usando recursion.
+    b) Definila utilizando la funcion map. 
+-}
+-- a)
+duplicar::[a]->[a] 
+duplicar [] = []
+duplicar (x:xs) = (x):(x):duplicar xs
+{-
+Ejecucion:
+ghci> duplicar [2,3]        Output: [2,2,3,3]
+ghci> duplicar ["a","b"]    Output: ["a","a","b","b"]
+-}
+-- b)
+{-
+Herramientas:
+    (\x -> replicate 2 x) es una funcion anonima que duplica x de la forma [x,x]
+-}
+duplicar' :: [a] ->[a]
+duplicar' xs = concat (map (\x -> replicate 2 x) xs)
+{-
+Ejecucion:
+ghci> duplicar' ["a"]       Output: ["a","a"]
+ghci> duplicar' ["a","b"]   Output: ["a","a","b","b"]
+-}
+
+----------------------------------------------------------------------------------------------
+{-
+Ejercicio 9:
+Programa una función que dada una lista de números xs, calcula una lista que tiene como
+elementos aquellos números de xs que son primos.
+    a) Defínala usando recursión.
+    b) Defínala utilizando la función filter.
+    c) Revisa tu definición del ejercicio 6g. ¿Cómo puedes mejorarla?
+-}
+
+-- a) 
+extraerPrimos ::[Int]->[Int]
+extraerPrimos [] = []
+extraerPrimos (x:xs) | (esPrimo x) = x:extraerPrimos xs
+                     | otherwise = extraerPrimos xs
+{-
+Ejecucion:
+ghci> extraerPrimos [2,1,5,4,3,11]                  Output: [2,5,3,11]
+ghci> extraerPrimos [2,1,5,4,3,11,19,23,44,33,77]   Output: [2,5,3,11,19,23]
+-}
+-----------------------------------------------------------------------------------------------
+-- b) 
+extraerPrimos' ::[Int]->[Int]
+extraerPrimos' xs = filter esPrimo xs
+{-
+Ejecucion:
+ghci> extraerPrimos' [2,1,5,4,3,11,19,23,44,33,77]  Output: [2,5,3,11,19,23]
+ghci> extraerPrimos' [2,1,5,4,3,11]                 Output: [2,5,3,11]
+-}
+
+-- c) Podria redefinir dicha funcion utilizando filter y productoria'
+multiplicarPrimos' ::[Int] -> Int
+multiplicarPrimos' xs = productoria' (filter esPrimo xs ) identidad
+{-
+Ejecucion:
+ghci> multiplicarPrimos' [3,5]              Output: 15
+ghci> multiplicarPrimos' [3,5,4,6,8]        Output: 15
+ghci> multiplicarPrimos' [3,5,4,6,8,11]     Output: 165
+-}
+
+---------------------------------------------------------------------------------------------
+{-
+Ejercicio 10:
+La función primIgualesA toma un valor y una lista, y calcula el tramo inicial más largo de
+la lista cuyos elementos son iguales a ese valor. Por ejemplo:
+                
+                primIgualesA 3 [3,3,4,1] = [3,3]
+                primIgualesA 3 [4,3,3,4,1] = []
+                primIgualesA 3 [] = []
+                primIgualesA 'a' "aaadaa" = "aaa"
+    
+    a) Programa primIgualesA mediante recursión.  
+    b) Programa nuevamente la función utilizando takeWhile.
+-}
+
 
