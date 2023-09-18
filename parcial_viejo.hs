002f6508f45def52f20bd5a7f299d08d9f18c2c2
pragmatically-dev <pragmatically.dev@gmail.com>
2023-09-17 22:54:06 -0300

    resolucion de parcial viejo

--- parcial_viejo.hs
+++ parcial_viejo.hs
@@ -23,3 +23,24 @@ False
 ghci> leGana Papel Piedra 
 True
 -}
+
+
+-- b)
+
+type Nombre = String 
+data Jugador = Mano Nombre Forma
+
+ganador :: Jugador -> Jugador -> Maybe Nombre
+ganador (Mano j1 Papel ) (Mano j2 Papel)  = Nothing
+ganador (Mano j1 Piedra) (Mano j2 Piedra) = Nothing
+ganador (Mano j1 Tijera) (Mano j2 Tijera) = Nothing
+ganador (Mano j1 f1) (Mano j2 f2) 
+                                |   (leGana f1 f2)      = Just j1
+                                |not(leGana f1 f2)      = Just j2  
+
+p1 = (Mano "Santiago" Papel)
+p2 = (Mano "X"     Piedra)
+{-
+ghci> ganador p1 p2
+Just "Santiago"
+-}
