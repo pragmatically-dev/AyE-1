1ec36c41a8c292334e77164e62e8dc28bdd190dd
pragmatically-dev <pragmatically.dev@gmail.com>
2023-09-17 22:41:10 -0300

    Intento de resolucion de parcial viejo

---
+++ parcial_viejo.hs
@@ -0,0 +1,16 @@
+
+--Ejercicio 1
+data Forma = Piedra | Papel | Tijera
+
+leGana ::Forma -> Forma -> Bool
+leGana Piedra Tijera = True
+leGana Piedra Piedra = False
+leGana Piedra Papel  = False
+
+leGana Papel Tijera  = False
+leGana Papel Piedra  = True
+leGana Papel Papel   = False
+
+leGana Tijera Tijera = False
+leGana Tijera Papel  = True
+leGana Tijera Piedra = False
